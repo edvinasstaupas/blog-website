2d9af183496ed946a54d19702bd593585ec09957
edvinasstaupas <staupas.edvinas@gmail.com>
2021-10-03 14:07:23 +0300

    PasswordEncoder as Bean

--- blog/src/main/java/lt/staupasedvinas/blog/config/SecurityConfig.java
+++ blog/src/main/java/lt/staupasedvinas/blog/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package lt.staupasedvinas.blog.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -52,6 +54,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .dataSource(dataSource)
                 .usersByUsernameQuery("select username, password, TRUE AS enabled from users where username = ?")
                 .authoritiesByUsernameQuery("select username, 'USER' AS authority from users where username = ?")
-                .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
